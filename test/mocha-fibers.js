4594df5a112c10304a1161caf1e6f5aa34395643
Adam Hull <adam@hmlad.com>
2014-06-26 19:08:16 -0700

    Failing test for suite timeout

--- test/mocha-fibers.js
+++ test/mocha-fibers.js
@@ -2,11 +2,13 @@ var assert = require('assert');
 var Fiber = require('fibers');
 
 describe('mocha-fibers', function(){
+  this.timeout(5000);
+
   beforeEach(function(){
-	assert(Fiber.current);
+  assert(Fiber.current);
   });
 
   it('should be in fiber', function(){
-	assert(Fiber.current);
+  assert(Fiber.current);
   });
 });
